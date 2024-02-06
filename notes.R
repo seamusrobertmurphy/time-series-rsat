a32b7f7efe42bbeab7733f2905f4153f10f036cc
seamusrobertmurphy <seamusrobertmurphy@gmail.com>
2024-02-06 04:23:07 -0800

    commit from new ppa

---
+++ notes.R
@@ -0,0 +1,11 @@
+
+
+
+#rsat notes.rmd
+
+{r}
+library(getSpatialData) 
+login_USGS(username = "seamusrobertmurphy@gmail.com")
+login_CopHub(username = "seamusrobertmurphy@gmail.com")
+login_earthdata(username = "seamusrobertmurphy@gmail.com")
+services()
